20fbdd2ef4cb983b18880ef2c26aaceec9f11774
Chema Gonzalez <chemag@fb.com>
2022-03-29 15:51:14 -0700

    micapp: fix app name
    App name is "com.facebook.micapp".
    
    Also modify app/build.gradle so apk contains full app name.
    ```
    ./app/build/outputs/apk/debug/com.facebook.micapp-v0.1-debug.apk
    ```

--- app/src/androidTest/java/com/facebook/micapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/facebook/micapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.facebook.microphonecheck", appContext.getPackageName());
+        assertEquals("com.facebook.micapp", appContext.getPackageName());
     }
 }
